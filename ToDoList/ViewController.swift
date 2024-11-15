8c8edf4f252833c94eab430c7cdb1baa011f4bc1
Alex <a.khodakow@gmail.com>
2024-11-15 06:37:22 +0300

    Create a project

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Алексей Ходаков on 15.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ ToDoListTests/ToDoListTests.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListTests.swift
+//  ToDoListTests
+//
+//  Created by Алексей Ходаков on 15.11.2024.
+//
+
+import XCTest
+@testable import ToDoList
+
+final class ToDoListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {}
+    }
+
+}
